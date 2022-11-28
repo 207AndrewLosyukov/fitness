cab506fc7a0f890922316570c7eed6a3a0953166
Andrew Losyukov <avlosyukov@edu.hse.ru>
2022-11-28 20:11:31 +0300

    init

---
+++ Sport/Activity.swift
@@ -0,0 +1,8 @@
+//
+//  Activity.swift
+//  Sport
+//
+//  Created by Андрей Лосюков on 28.11.2022.
+//
+
+import Foundation
---
+++ Sport/TypeActivity.swift
@@ -0,0 +1,8 @@
+//
+//  TypeActivity.swift
+//  Sport
+//
+//  Created by Андрей Лосюков on 28.11.2022.
+//
+
+import Foundation
